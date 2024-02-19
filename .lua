256e3865fdf1530f33e482650b839ec1915706f7
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-02-19 17:53:44 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,7 @@
 local Library = 
 loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 local Window_1 = Library:NewWindow("Beta Turtle Hub UI")
-
+local user = game:GetService("Players").LocalPlayer
 
 local Tab1 = Window_1:NewSection("Main")
 local Tab2 = Window_1:NewSection("Bullet settings")
@@ -12,6 +12,18 @@ local function playerInst(func)
   end
 end
 
+local function shoot(position)
+    game:GetService("Players")["LocalPlayer"]["Character"]["Shuriken"]["HitEvent"]:FireServer(position)
+end
+
+local function detectWallAndShoot()
+    local ray = Ray.new(user.Character.Head.Position,user.Character.Head.CFrame.LookVector * 500)
+    local hitPart, hitPosition = game.Workspace:FindPartOnRay(ray,user.Character,false,true)
+    if hitPart and hitPart:IsA("BasePart") or hitPart:IsA("Part") then
+        shoot(hitPosition)
+    end
+end
+
 Tab1:CreateToggle("Auto farm ninjutsu",function(value)
         _G.ninj = value
     while wait() do
@@ -20,12 +32,18 @@ Tab1:CreateToggle("Auto farm ninjutsu",function(value)
     end
 end)
 
+Tab2:CreateToggle("Wallbang",function(value)
+    _G.wallb = value
+end)
+
 Tab1:CreateToggle("Auto spam shuriken",function(value)
     _G.sshu = value
     while wait() do
       if _G.sshu == false then break end
       playerInst(function(i,v)
-        game:GetService("Players")["LocalPlayer"]["Character"]["Shuriken"]["HitEvent"]:FireServer(v.Position)
+          if _G.wallb == false then
+            shoot(v.Position)
+          end
       end)
   end
 end)
